e8cef34c59096f3cdf534b6109376ff364d5f7de
xcvtt <exclide1@gmail.com>
2026-03-11 11:34:17 +0300

    [cs2ext] add vibe dumper

---
+++ dmp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OUTPUT="project_dump.txt"
+IGNORE="cmake*|.idea|.git|dmp.sh|project_dump.txt"
+
+echo "Project is cs2 external read-only cheat with features: ESP, radar, spectator list, config saving/loading, imgui menu, crosshair, parsing offsets in format of cs2 dumper, chams like ESP etc" > "$OUTPUT"
+echo "" >> "$OUTPUT"
+echo "Project structure:" >> "$OUTPUT"
+tree -I "$IGNORE" >> "$OUTPUT"
+
+echo "" >> "$OUTPUT"
+echo "Now I'll list the project files:" >> "$OUTPUT"
+echo "" >> "$OUTPUT"
+
+tree -fi -I "$IGNORE" | grep -E '\.(cpp|h)$|CMakeLists.txt$' | while read -r f; do
+    echo "FILE: $f" >> "$OUTPUT"
+    cat "$f" >> "$OUTPUT"
+    echo "" >> "$OUTPUT"
+    echo "" >> "$OUTPUT"
+done
+
+echo "Export written to $OUTPUT"
